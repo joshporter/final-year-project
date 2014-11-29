eff5f7ff643d168ddd5fe6c10e4db0123a978114
Josh Porter <josh@joshporter.co.uk>
2014-11-29 12:53:35 +0000

    Refactor template out into own file

--- src/input/input_controls.directive.js
+++ src/input/input_controls.directive.js
@@ -1,33 +1,25 @@
 function inputControls() {
-    var start = angular.element("<button class='btn btn-default glyphicon glyphicon-play' ng-click='vm.play()'></button>"),
-        stop =  angular.element("<button class='btn btn-default glyphicon glyphicon-stop' disabled ng-click='vm.stop()'></button>"),
-        liveInput = angular.element("<button class='btn btn-default glyphicon glyphicon-record' data-playing ng-click='vm.liveInput()'></button>");
-
-    this.link = function (scope, element) {
-        start.on('click', function(){
-           stop.prop('disabled', false);
-           start.prop('disabled', true);
-        });
-
-        stop.on('click', function(){
-            start.prop('disabled', false);
-            stop.prop('disabled', true);
-        });
-
-        liveInput.on('click', function(){
-            liveInput.toggleClass("btn-danger");
-        });
-    }
-
     return {
         restrict: 'EA',
-        replace: true,
-        compile: function (tElem) {
-            tElem.append(start);
-            tElem.append(stop);
-            tElem.append(liveInput);
+        templateUrl: 'templates/controls.html',
+        link: function (scope, element) {
+            var start = angular.element('.glyphicon-play'),
+                stop = angular.element('.glyphicon-stop'),
+                liveInput = angular.element('.glyphicon-record');
 
-            return link;
+            start.on('click', function(){
+                stop.prop('disabled', false);
+                start.prop('disabled', true);
+            });
+
+            stop.on('click', function(){
+                start.prop('disabled', false);
+                stop.prop('disabled', true);
+            });
+
+            liveInput.on('click', function(){
+                liveInput.toggleClass("btn-danger");
+            });
         }
     };
 }
